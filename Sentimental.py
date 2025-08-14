95163bdf98db228f8bb286e9e570979656df97d2
Daniel-Tech2014 <felixajayidaniel@gmail.com>
2025-08-14 17:27:55 +0100

    Add files via upload

---
+++ Sentimental.py
@@ -0,0 +1,12 @@
+import colorama
+from colorama import Fore Style
+From textblob import Textblob
+colorama.init
+("Welcome to Sentiment Spy")
+username=input("Enter your name:")M
+if not username:
+  username="MysteryAgent"
+  conversation_history=[]
+  print("Hello Agent")
+  print("Type a Sentence I will anaylize")
+  print("Type ")
